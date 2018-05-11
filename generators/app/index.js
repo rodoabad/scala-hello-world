9d698fb652443025a7f7440bd048ffaf3fd142e6
rodoabad <rodoabad@gmail.com>
2018-05-11 07:23:38 -0500

    chore: add commit linting through `husky`

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,11 +6,11 @@ module.exports = class extends Generator {
   }
 
   method1() {
-    this.log('method 1 just ran');
+    this.log('method 1 just ran!');
   }
 
   method2() {
-    this.log('method 2 just ran');
+    this.log('method 2 just ran!');
   }
   writing() {
     this.fs.copyTpl(
